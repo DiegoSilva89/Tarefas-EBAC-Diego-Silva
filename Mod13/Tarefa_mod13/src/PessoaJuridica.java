6aa72a26e57a76d8de6b8a71c3a31d4053668e60
DiegoSilva89 <diegosantosdasilva89@gmail.com>
2023-03-30 15:24:33 -0300

    Tarefa Módulo 13

---
+++ Mod13/Tarefa_mod13/src/PessoaJuridica.java
@@ -0,0 +1,12 @@
+public class PessoaJuridica extends Pessoa {
+    private String cnpj;
+
+    public PessoaJuridica(String nome, String cnpj) {
+        super(nome);
+        this.cnpj = cnpj;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+}
